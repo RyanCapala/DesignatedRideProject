455f846e6fd4e3a24a7b26ea7051613808bb9520
perrinod <perrinod@unlv.nevada.edu>
2018-02-20 23:03:40 -0800

    Update DriverActivity.java
    Using a button as a temporary location to enable a driver to become available for picking someone up. So the Driver can become available to the Rider in RiderMapActivity.

--- DesignatedRide/app/src/main/java/com/example/awesomeness/designatedride/Activities/DriverActivity.java
+++ DesignatedRide/app/src/main/java/com/example/awesomeness/designatedride/Activities/DriverActivity.java
@@ -1,7 +1,9 @@
+
 package com.example.awesomeness.designatedride.Activities;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -13,12 +15,21 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.awesomeness.designatedride.R;
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -29,6 +40,18 @@ public class DriverActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabaseReference;
     private FirebaseDatabase mDatabase;
+
+    private String _Driver = "Driver";
+    private String _isAvaliable = "isAvaliable";
+    private String userid;
+    private String _AvaliableGeolocation = "AvaliableGeoLocation";
+    private String _geoKey = "geoKey";
+    private String key = "";
+
+    private GeoFire mGeoFire;
+    private DatabaseReference mGeoLocationRef;
+
+    private FusedLocationProviderClient mLocation;
 
     private String cUserProfileImage = "userImage";
 
@@ -51,9 +74,13 @@ public class DriverActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
+        userid = mAuth.getCurrentUser().getUid();
         mDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mDatabase.getReference();
+        mGeoLocationRef = FirebaseDatabase.getInstance().getReference().child(_AvaliableGeolocation);
+        mGeoFire = new GeoFire(mGeoLocationRef);
         mDatabaseReference.keepSynced(true);
+
 
         //------------------------
         profileBtn.setOnClickListener(new View.OnClickListener() {
@@ -68,7 +95,61 @@ public class DriverActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (isServicesOk()) {
-                    gotoActivityWithArrowBack(DriverMapActivity.class);
+                    //temporary using this button as a trigger to make the Driver avaliable for queries for RiderMapActivity.
+                    mDatabaseReference.child(_Driver).child(userid).child(_geoKey).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            key = dataSnapshot.getValue(String.class);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+                    mLocation = LocationServices.getFusedLocationProviderClient(DriverActivity.this);
+                    mLocation.getLastLocation().addOnSuccessListener(DriverActivity.this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            mGeoFire.setLocation(key, new GeoLocation(location.getLatitude(), location.getLongitude()), new GeoFire.CompletionListener() {
+                                @Override
+                                public void onComplete(String key, DatabaseError error) {
+                                    mDatabaseReference.child(_AvaliableGeolocation).child(key).child(_isAvaliable).setValue("true");
+                                }
+                            });
+                        }
+                    });
+
+                    mDatabaseReference.child(_AvaliableGeolocation).child(key).addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                        }
+
+                        @Override
+                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                            if(dataSnapshot.getKey().equals(_isAvaliable)) {
+                                mDatabaseReference.child(_AvaliableGeolocation).child(key).removeValue();
+                                gotoActivity(DriverMapActivity.class);
+                            }
+                        }
+
+                        @Override
+                        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
                 }
             }
         });

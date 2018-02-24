61e542a1af87fbc53190bb83c5eb48fe6339bd84
Ryan Capala <capala@unlv.nevada.edu>
2018-02-23 21:13:06 -0800

    Added popup dialog borders for profile and confirmation

--- DesignatedRide/app/src/main/java/com/example/awesomeness/designatedride/Model/ProfileDialogHelper.java
+++ DesignatedRide/app/src/main/java/com/example/awesomeness/designatedride/Model/ProfileDialogHelper.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -55,6 +57,7 @@ public class ProfileDialogHelper {
 
         dialogBuilder.setView(profilePopupView);
         dialog = dialogBuilder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
 
         updateProfileBtn.setOnClickListener(new View.OnClickListener() {
@@ -93,6 +96,7 @@ public class ProfileDialogHelper {
 
         _dialogBuilder.setView(confirmationView);
         confirmationDialog = _dialogBuilder.create();
+        confirmationDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         confirmationDialog.show();
 
         yesButton.setOnClickListener(new View.OnClickListener() {

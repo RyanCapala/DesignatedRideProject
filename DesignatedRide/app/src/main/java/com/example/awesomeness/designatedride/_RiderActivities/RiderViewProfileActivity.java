7e13471f107558366ff8edc0e5a99f13425b030d
Matt Brown <matthew.nic.brown@gmail.com>
2018-02-27 01:15:56 -0800

    Added helper function for filling user data
    - Still need to retrieve user data

--- DesignatedRide/app/src/main/java/com/example/awesomeness/designatedride/_RiderActivities/RiderViewProfileActivity.java
+++ DesignatedRide/app/src/main/java/com/example/awesomeness/designatedride/_RiderActivities/RiderViewProfileActivity.java
@@ -2,26 +2,61 @@ package com.example.awesomeness.designatedride._RiderActivities;
 
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.awesomeness.designatedride.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RiderViewProfileActivity extends AppCompatActivity {
+    private static final String TAG = "RiderViewProfileActivity";
+
+    // Firebase
+    private FirebaseUser mUser;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabaseReference;
+    private FirebaseDatabase mDatabase;
+
+    // Widgets
+    ImageButton editProfileButton;
+
+    private enum riderInfoItem{
+        FIRST_NAME, LAST_NAME, AGE, EMAIL, PHONE_NUMBER,
+        INSURANCE_CO, RIDE_COVERAGE, DOCTOR_GEN_NAME,
+        PUBLIC_NAME, WHEEL_CHAIR_REQ
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider_view_profile);
         initWidgets();
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference();
+        mDatabaseReference.keepSynced(true);
+
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(RiderViewProfileActivity.this, "Edit profile button pressed!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     private void initWidgets(){
         initLabels();
+        editProfileButton = findViewById(R.id.editProfileImageButton_rider);
     }
 
     private void initLabels(){
@@ -48,9 +83,36 @@ public class RiderViewProfileActivity extends AppCompatActivity {
         setTabularLabelText((ConstraintLayout) findViewById(R.id.viewProfileRider_public_wheelChairReq),
                 getResources().getString(R.string.rider_profile_public_info_wheelchair_required));
     }
+
+    // TODO: 2/27/2018 Retreive relevent data from database, whether local or Firebase 
+    private void retrieveUserData(){
+
+    }
+    private void setTabularItemText(riderInfoItem itemType, String value){
+        int id = -1;
+        switch (itemType){
+            case FIRST_NAME: id = R.id.viewProfileRider_gen_fName; break;
+            case LAST_NAME: id = R.id.viewProfileRider_gen_lName; break;
+            case AGE: id = R.id.viewProfileRider_gen_age; break;
+            case EMAIL: id = R.id.viewProfileRider_gen_age; break;
+            case PHONE_NUMBER: id = R.id.viewProfileRider_gen_phoneNumber; break;
+            case INSURANCE_CO: id = R.id.viewProfileRider_med_insuranceCompany; break;
+            case RIDE_COVERAGE: id = R.id.viewProfileRider_med_riderCoverage; break;
+            case DOCTOR_GEN_NAME: id = R.id.viewProfileRider_med_doctorGenName; break;
+            case PUBLIC_NAME: id = R.id.viewProfileRider_public_publicName; break;
+            case WHEEL_CHAIR_REQ: id = R.id.viewProfileRider_public_wheelChairReq; break;
+        }
+        if(id != -1 && value != null){
+            TextView valueText = findViewById(id);
+            valueText.setText(value);
+        }
+
+    }
     private void setTabularLabelText(ConstraintLayout layout, String text){
         TextView label = (TextView)layout.getChildAt(0);
         label.setText(text);
     }
+
+
 
 }

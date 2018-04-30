4361f0006d571e410cc9f5ae39a0fd0cf21aa4ba
RyanCapala <capala@unlv.nevada.edu>
2018-04-30 05:52:25 -0700

    Added backarrow button and Title on RiderViewAppointmentActivityWrapper

--- DesignatedRide/app/src/main/java/com/example/awesomeness/designatedride/_RiderActivities/RiderViewAppointmentActivityWrapper.java
+++ DesignatedRide/app/src/main/java/com/example/awesomeness/designatedride/_RiderActivities/RiderViewAppointmentActivityWrapper.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -32,6 +33,9 @@ public class RiderViewAppointmentActivityWrapper extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rider_view_appointment_wrapper);
+
+        // For Back Arrow
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         //
@@ -127,6 +131,17 @@ public class RiderViewAppointmentActivityWrapper extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(new Intent(getApplicationContext(), RiderActivity.class));
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private String[] buildString(List<String> sList) {
